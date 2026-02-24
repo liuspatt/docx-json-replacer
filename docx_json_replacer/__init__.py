b487b38983ff5a19f09984c4243a0c4a921677fd
Luis Patt <liuspatt@gmail.com>
2026-02-24 12:04:11 -0500

    chore: Update version to 0.9.1 and fix package build configuration for PyPI deployment

--- docx_json_replacer/__init__.py
+++ docx_json_replacer/__init__.py
@@ -3,5 +3,5 @@ from .table_handler import TableHandler
 from .formatting_handler import FormattingHandler
 from .image_handler import ImageHandler
 
-__version__ = "0.9.0"
+__version__ = "0.9.1"
 __all__ = ["DocxReplacer", "replace_docx_template", "TableHandler", "FormattingHandler", "ImageHandler"]
